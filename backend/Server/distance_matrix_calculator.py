c7ae0337925e4791447c089b9fe12ce96dbd91b8
David Weber <davidweber@Davids-MacBook-Pro.local>
2017-12-02 22:59:39 +0100

    distance_matrix_calculator working in Flask

--- backend/Server/distance_matrix_calculator.py
+++ backend/Server/distance_matrix_calculator.py
@@ -3,14 +3,16 @@ from flask import Flask, flash, render_template, url_for, request, redirect, ses
 import os
 from datetime import datetime
 import googlemaps
+from ast import literal_eval
 gmaps = googlemaps.Client(key='AIzaSyB8HBULX1Kov_bhrDnhi9XrT8N0L3kjscw')
 
 
-@app.route('/calculate')
-def generate_tuples(centerTuple):
+@app.route('/calculate', methods=['POST', 'GET'])
+def generate_tuples():
+	latitude=float(request.args.get('latitude'))
+	longitude=float(request.args.get('longitude'))
+	centerTuple=(latitude,longitude)
 	step=0.01
 	gridSize=(-5,5)
-	return [(centerTuple[0]+i*step,centerTuple[1]+j*step) for i in range(gridSize[0], gridSize[1]) for j in range(gridSize[0],gridSize[1])]
-
-def matrix(centerTuple):
-	return gmaps.distance_matrix(centerTuple, generate_tuples(centerTuple), mode='walking')
+	Matrix = [(centerTuple[0]+i*step,centerTuple[1]+j*step) for i in range(gridSize[0], gridSize[1]) for j in range(gridSize[0],gridSize[1])]
+	return str(gmaps.distance_matrix(centerTuple, Matrix, mode='walking'))
