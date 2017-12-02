5e460ca7f6404ff0cb8a91d91ad24f9123c6816f
David Weber <davidweber@Davids-MacBook-Pro.local>
2017-12-02 19:46:19 +0100

    app.py

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, flash, render_template, url_for, request, redirect, session
+import os
+
+
+
+
+
+app = Flask(__name__)
+app.secret_key = '1234'
+
+app.debug = True
+
+
+app.config.from_object(__name__)
+
+
+
+@app.route('/')
+def home():
+        return 'Hello ArcGIS'
+
+
+if __name__ == "__main__":
+        app.run(debug=True,host='0.0.0.0', port=3000)
