fc6c7b2cb6b83e1e26f4924d37fb4d725c58ed1b
David Weber <davidweber@Davids-MacBook-Pro.local>
2017-12-03 01:14:40 +0100

    Json return includes Longitude and Latitude

--- backend/Server/distance_matrix_calculator.py
+++ backend/Server/distance_matrix_calculator.py
@@ -4,16 +4,16 @@ import os
 from datetime import datetime
 import googlemaps
 from ast import literal_eval
-gmaps = googlemaps.Client(key='AIzaSyB8HBULX1Kov_bhrDnhi9XrT8N0L3kjscw')
+gmaps = googlemaps.Client(key='AIzaSyA2eFpbH0vjcilxHTCx0l0dUYTKqJwwgZ4')
 
 @app.route('/calculate', methods=['POST', 'GET'])
 def generate_tuples():
 	latitude=float(request.args.get('latitude'))
 	longitude=float(request.args.get('longitude'))
 	centerTuple=(latitude,longitude)
-	step=0.01
-	gridSize=(-5,5)
+	step=0.9
+	gridSize=(-3,3)
 	Matrix = [(centerTuple[0]+i*step,centerTuple[1]+j*step) for i in range(gridSize[0], gridSize[1]) for j in range(gridSize[0],gridSize[1])]
 	distance_matrix=gmaps.distance_matrix(centerTuple, Matrix, mode='walking')
-	
-	return jsonify(distance_matrix)
+	Matrix = {'Coordinates': Matrix}
+	return jsonify(Matrix, distance_matrix)
